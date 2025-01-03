8e7fce9d81ddb48d788d0d546534788f90062602
ItzzLuk <hopplukas8@gmail.com>
2025-01-03 20:09:57 +0100

    Update DBK32functions.pas

--- Cheat Engine/dbk32/DBK32functions.pas
+++ Cheat Engine/dbk32/DBK32functions.pas
@@ -3191,19 +3191,19 @@ begin
         if not fileexists(dataloc) then
         begin
 
-          servicename:='CEDRIVER73';
+          servicename:='sethioz';
           ultimapservicename:='ULTIMAP2';
           processeventname:='DBKProcList60';
           threadeventname:='DBKThreadList60';
 
           if iswow64 then
           begin
-            sysfile:='dbk64.sys';
+            sysfile:='sethioz64.sys';
             ultimapsysfile:='ultimap2-64.sys';
           end
           else
           begin
-            sysfile:='dbk32.sys';
+            sysfile:='sethioz32.sys';
             ultimapsysfile:='';
           end;
 
